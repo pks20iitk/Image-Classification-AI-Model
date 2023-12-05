8989cfca8946d2ca3346299f8a1000eb3c7f9ebe
Prince <princ.brainalive@gmail.com>
2023-12-05 20:00:39 +0530

    added logger and exception in utils file

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.cnnClassifier import logger
+
+
+
+logger.info("Welcom to Image-classification-AI-Model-Project")
--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "Chest-Cancer-Classification-Project"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "Image-Classification-AI-Model"
+AUTHOR_USER_NAME = "pks20iitk"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "prince.brainalive@gmail.com"
 
 
 setuptools.setup(
