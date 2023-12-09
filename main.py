ff177754c8149e63cdea6353e39d92df78b0148f
Prince <princ.brainalive@gmail.com>
2023-12-09 19:00:13 +0530

    modify dockerfile

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from src.cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseMo
 from src.cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 from src.cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-
-
 STAGE_NAME = "Data Ingestion Stage"
-
-
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -18,21 +14,18 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
-    
 
 STAGE_NAME = "Prepare base model"
 
-
-try: 
-   logger.info(f"*******************")
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-   prepare_base_model = PrepareBaseModelTrainingPipeline()
-   prepare_base_model.main()
-   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
-
+    logger.exception(e)
+    raise e
 
 STAGE_NAME = "Training"
 try:
